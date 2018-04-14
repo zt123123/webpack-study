8e67490f2988ede021dcdcacac18f7e08e2c9628
zt123123 <249812928@qq.com>
2018-04-14 22:43:15 +0800

    add new file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor(name, age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    say() {
+        console.log(this.name);
+    }
+    hello() {
+        console.log("我今年" + this.age);
+    }
+}
+
+let person = new Person('张天', 23)
+person.say();
+person.hello()
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractText = require('extract-text-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+
+const pathResole = paths => path.resolve(__dirname, paths);
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: pathResole('dist'),
+        filename: '[name][hash:6].js',
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.less$/,
+                loader: ['less-loader']
+            },
+            {
+                test: /\.(jpg|png|gif)$/i,
+                loader: 'file-loader',
+            }
+        ]
+    },
+    resolve: {},
+    devServer: {
+        host: '192.168.1.121',
+        open: true,
+        hot: true
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CleanPlugin(['dist']),
+        new HtmlPlugin({
+            minify: {
+                collapseWhitespace: true
+            },
+            template: pathResole('index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
